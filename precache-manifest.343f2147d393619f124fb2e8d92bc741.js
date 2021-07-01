cd356f7a1ce5e5ff1b5fca8983e49382a87cda6a
ybroyde <59289827+ybroyde@users.noreply.github.com>
2021-07-01 16:51:18 -0400

    Add files via upload

---
+++ precache-manifest.343f2147d393619f124fb2e8d92bc741.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8180fbf4c6dc93b013e3a764e130b056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2814c68d719fa0ec9447",
+    "url": "/static/css/main.d13432f0.chunk.css"
+  },
+  {
+    "revision": "57a0f5df15371b303153",
+    "url": "/static/js/2.01687038.chunk.js"
+  },
+  {
+    "revision": "2814c68d719fa0ec9447",
+    "url": "/static/js/main.7e8e7c99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
